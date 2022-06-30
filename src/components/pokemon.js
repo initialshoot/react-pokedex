2c0ac2385cdffa46ca1a484ff92989a36c58e26c
Omar_asc <initialshoot@outlook.com>
2022-06-30 12:15:29 -0500

    Style: add new style to the pokemons cards

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -7,26 +7,25 @@ const Pokemon = (props) => {
     return (
 
         <div className="pokemon-card">
-            <div className="pokemon-img">
-                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <div className="pokemon-img-container">
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} className="pokemon-img"/>
             </div>
-            <div>
-                <div>
+            <div className="card-body">
+                <div className="card-top">
                     <h3>{pokemon.name}</h3>
                     <div>#{pokemon.id}</div>
                 </div>  
-                    <div>
+                <div className="card-bottom">
+                    <div className="pokemon-type">
                         {pokemon.types.map((type, idx) => {
-                            return <div key={idx}>{type.name}</div>;
+                            return <div key={idx} className="pokemon-type-text">{type.type.name}</div>;
                         })}
                     </div>
-                    <div>♥️</div>
-                <div>
-
+                    <div className="pokemon-favorite">❤️</div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Pokemon;
