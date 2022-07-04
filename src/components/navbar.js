2db9e50f7b7cecad80d4111e4307c70a1882370e
Omar_asc <initialshoot@outlook.com>
2022-07-04 00:59:37 -0500

    Feat add new options to the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Navbar = () => {
 
     let imgUrl = "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
@@ -8,18 +9,29 @@ const Navbar = () => {
 
         <nav>
 
-            <div>
+                <div className='options'>
 
-                <button className='home-btn'>
-                    <img 
-                    src={imgUrl}
-                    alt="Logo Pokeapi"
-                    className='navbarImg'
-                    />
-                </button>
-                
-            </div>
-            
+                    <a className='home-btn' href="/">
+                        <img 
+                        src={imgUrl}
+                        alt="Logo Pokeapi"
+                        className='navbarImg'
+                        />
+                    </a>
+
+                    <a href="/" className='hover-underline-animation'>
+                        Home
+                    </a>
+
+                    <a href="/pokemons" className='hover-underline-animation'>
+                        Pokemons
+                    </a>
+
+                    <a heref="" className='hover-underline-animation'>
+                        About
+                    </a>
+
+                </div>
 
         </nav>
 
